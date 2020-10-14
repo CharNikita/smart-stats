d7cd6dd77b6bd4b1a775899d754b162692829c6c
nhoncharenko <vWWaX1Kd0ZIjym2>
2020-10-15 02:50:07 +0700

    add more options

--- modal/index.js
+++ modal/index.js
@@ -4,6 +4,19 @@ import {Modal} from './source/modal.js';
 const modal = new Modal('#modal', {
     title: "title",
     closable: true,
+    components: [],
+    footerButtons: [
+        {
+            name: 'ok', handler() {
+                console.log('ok is clicked')
+            }
+        },
+        {
+            name: 'deny', handler() {
+                console.log('deny is clicked')
+            }
+        }
+    ]
 });
 
 modal.open()
--- modal/source/modal.js
+++ modal/source/modal.js
@@ -17,29 +17,43 @@ export class Modal {
     };
 
     addComponent(component) {
-
-    };
-
-    deleteComponent() {
-
+        this.element.querySelector('[data-body]').appendChild(component);
     };
 
     destroy() {
-        this.element.innerHTML = '';
+        this.element.parentNode.removeChild(this.element);
+        this.element.removeEventListener('click', this.#clickListener);
     };
 
     #setup() {
-        this.element.addEventListener('click', event => {
-            if (event.target.dataset.close) {
-                this.close();
-            }
-        })
+        this.element.addEventListener('click', this.#clickListener);
     };
 
     #render() {
         this.element.classList.add('modal');
         this.element.innerHTML = this.#getModalTemplate();
+        this.#makeFooter(this.options.footerButtons);
     };
+
+    #clickListener = event => {
+        if (event.target.dataset.close) {
+            this.close();
+        }
+    }
+
+    #makeFooter(buttons = []) {
+        for (const button of buttons) {
+            this.element.querySelector('[data-footer]').appendChild(this.#makeButton(button));
+        }
+    }
+
+    //  TODO: add default style
+    #makeButton(btn) {
+        const button = document.createElement('button');
+        button.innerText = btn.name ?? 'name';
+        button.addEventListener('click', btn.handler ?? new Function());
+        return button;
+    }
 
     #getModalTemplate() {
         return `<div class="modal-overlay" data-close="true">
@@ -48,12 +62,8 @@ export class Modal {
                         <span class="modal-title">${this.options.title ?? 'Title'}</span>
                         ${this.options.closable ? `<span class="modal-close" data-close="true">&times;</span>` : ''}
                     </div>
-                    <div class="modal-body">
-                        
-                    </div>
-                    <div class="modal-footer">
-                        
-                    </div>
+                    <div class="modal-body" data-body></div>
+                    <div class="modal-footer" data-footer></div>
                 </div>
             </div>`
     }
